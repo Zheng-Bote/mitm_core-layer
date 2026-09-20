ebb903f28da680627ce6566b39309a231be772cf
Zheng-Bote <zheng.bote@googlemail.com>
2026-09-20 16:40:05 +0200

    upd

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ mkdir -p ../bin
 
 # Copy the binaries
 echo "Copying binaries to ../bin/"
-cp target/x86_64-unknown-linux-musl/release/mitm-http-server ../bin/
-cp target/x86_64-unknown-linux-musl/release/mitm-iam-server ../bin/
-cp target/x86_64-unknown-linux-musl/release/mitm-scheduler-server ../bin/
+cp target/x86_64-unknown-linux-musl/release/mitm-http-server ../bin/mitm-core-http
+cp target/x86_64-unknown-linux-musl/release/mitm-iam-server ../bin/mitm-core-iam
+cp target/x86_64-unknown-linux-musl/release/mitm-scheduler-server ../bin/mitm-iam-scheduler
 
 echo "Build complete! Binaries are located in ../bin/"
